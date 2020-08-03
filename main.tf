32253c41fea0baa51da93814cf72a41b1cbfe224
Iceal Lim <iceal@accurics.com>
2020-08-03 06:22:04 +0000

    main.tf edited online with Bitbucket

--- main.tf
+++ main.tf
@@ -6,6 +6,10 @@ resource "aws_s3_bucket" "demo-s3" {
   bucket = "${var.s3_bucket_name}-demo"
 }
 
+resource "aws_s3_bucket" "demo-s3-b" {
+  bucket = "${var.s3_bucket_name}-demob"
+}
+
 variable "s3_bucket_name" {
   default = "demo-2020-07-29"
 }
