5330e1281baf24a880e91dcf0ae1f3ad6bacaf2c
Iceal Lim <iceal@accurics.com>
2020-09-05 00:37:13 +0000

    main.tf edited online with Bitbucket

--- main.tf
+++ main.tf
@@ -4,6 +4,13 @@ provider "aws" {
 
 resource "aws_s3_bucket" "demo-s3" {
   bucket = "${var.s3_bucket_prefix}-terraformcloud"
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }  
 }
 
 variable "s3_bucket_prefix" {
