56185bd14903568c1b9a89ea78b14e79f2ba39dc
Iceal Lim <iceal@accurics.com>
2020-09-14 18:51:23 +0000

    Fix

--- main.tf
+++ main.tf
@@ -7,6 +7,13 @@ resource "aws_s3_bucket" "demo-s3" {
   versioning {
     enabled = false
   }
+  "server_side_encryption_configuration": {
+    "rule": {
+      "apply_server_side_encryption_by_default": {
+        "sse_algorithm": "AES256"
+      }
+    }
+  }
 }
 
 variable "s3_bucket_prefix" {
