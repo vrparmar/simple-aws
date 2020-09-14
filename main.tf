c0d8d3389a538ffc771c83aa087c371038ccc608
Iceal Lim <iceal@accurics.com>
2020-09-14 18:48:17 +0000

    main.tf edited online with Bitbucket

--- main.tf
+++ main.tf
@@ -7,13 +7,6 @@ resource "aws_s3_bucket" "demo-s3" {
   versioning {
     enabled = false
   }
-  server_side_encryption_configuration {
-    rule {
-      apply_server_side_encryption_by_default {
-        sse_algorithm = "AES256"
-      }
-    }
-  }  
 }
 
 variable "s3_bucket_prefix" {
