fcd7d90168f02b4a26dee97e734b8b289575f79c
Iceal Lim <iceal@accurics.com>
2020-12-01 08:03:54 +0000

    main.tf edited online with Bitbucket

--- main.tf
+++ main.tf
@@ -31,19 +31,3 @@ resource "local_file" "vpc-id" {
   JSON
   filename = "./vpc-id.json"
 }
-
-
-# provider "aws" {
-#   region = "us-east-1"
-# }
-
-# resource "aws_s3_bucket" "demo-s3" {
-#   bucket = "${var.s3_bucket_prefix}-terraformcloud"
-#   versioning {
-#     enabled = false
-#   }
-# }
-
-# variable "s3_bucket_prefix" {
-#   default = "prefix20201112"
-# }
