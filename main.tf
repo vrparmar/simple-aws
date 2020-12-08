dc4feb411ff84c5d0ea953237a3a5be3ccf00285
Iceal Lim <iceal@accurics.com>
2020-12-08 15:36:21 -0800

    adding s3 bucket

--- main.tf
+++ main.tf
@@ -9,25 +9,25 @@ resource "aws_s3_bucket" "bucket-example" {
   bucket = "bucket-example-20201112"
 }
 
-resource "null_resource" "accurics_notify" {
-    triggers = {
-        any = timestamp()
-    }
+# resource "null_resource" "accurics_notify" {
+#     triggers = {
+#         any = timestamp()
+#     }
     
-    provisioner "local-exec" {
-        command = "chmod +x accurics-notify.sh && ./accurics-notify.sh"
-    }
-}
+#     provisioner "local-exec" {
+#         command = "chmod +x accurics-notify.sh && ./accurics-notify.sh"
+#     }
+# }
 
-data "aws_vpc" "accurics_vpc" {
-  id = "vpc-00ab82b3d0d23ff34"
-}
+# data "aws_vpc" "accurics_vpc" {
+#   id = "vpc-00ab82b3d0d23ff34"
+# }
 
-resource "local_file" "vpc-id" {
-  content  = <<JSON
-{
-  "vpc_id": data.aws_vpc.accurics_vpc.id
-}
-  JSON
-  filename = "./vpc-id.json"
-}
+# resource "local_file" "vpc-id" {
+#   content  = <<JSON
+# {
+#   "vpc_id": data.aws_vpc.accurics_vpc.id
+# }
+#   JSON
+#   filename = "./vpc-id.json"
+# }
