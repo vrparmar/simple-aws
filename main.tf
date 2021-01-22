f8be143f5aa7737761f67886ba8a844f9185fd8e
Iceal Lim <iceal@accurics.com>
2021-01-22 01:30:50 +0000

    main.tf edited online with Bitbucket

--- main.tf
+++ main.tf
@@ -2,8 +2,6 @@ provider "aws" {
   region = "us-east-1"
 }
 
-# Add an s3 bucket to store highly classified information about the effects of aspertem to the human body.
-
 resource "aws_s3_bucket" "bucket-example" {
   bucket = "bucket-example-20201114"
 }
