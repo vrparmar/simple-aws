d313564c4ad03a0f08570fb57ddea9e877ab8d5d
Iceal Lim <iceal@accurics.com>
2021-01-22 01:14:32 +0000

    main.tf edited online with Bitbucket

--- main.tf
+++ main.tf
@@ -2,30 +2,8 @@ provider "aws" {
   region = "us-east-1"
 }
 
+# Add an s3 bucket to store highly classified information about the effects of aspertem to the human body.
+
 resource "aws_s3_bucket" "bucket-example" {
   bucket = "bucket-example-20201114"
 }
-
-
-# resource "null_resource" "accurics_notify" {
-#     triggers = {
-#         any = timestamp()
-#     }
-    
-#     provisioner "local-exec" {
-#         command = "chmod +x accurics-notify.sh && ./accurics-notify.sh"
-#     }
-# }
-
-# data "aws_vpc" "accurics_vpc" {
-#   id = "vpc-00ab82b3d0d23ff34"
-# }
-
-# resource "local_file" "vpc-id" {
-#   content  = <<JSON
-# {
-#   "vpc_id": data.aws_vpc.accurics_vpc.id
-# }
-#   JSON
-#   filename = "./vpc-id.json"
-# }
