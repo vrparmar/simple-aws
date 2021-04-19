a202524a453475c979898880080bd1aaf523b25a
Harshit Omar <harshit@accurics.com>
2021-04-19 16:47:26 +0000

    main.tf edited online with Bitbucket

--- main.tf
+++ main.tf
@@ -2,10 +2,34 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_s3_bucket" "bucket-example" {
+resource "aws_security_group" "harshit_sg" {
+  name        = "harshit_sg"
+  description = "Used in the terraform"
+  vpc_id      = "harshit-vpc"
 
+  tags = {
+    Name = "harshit_sg"
+  }
 
-  bucket = "bucket-example-20210121"
-  
-  
+  ingress {
+    to_port     = 3020
+    from_port   = 3020
+    protocol    = "tcp"
+    cidr_blocks = ["192.164.0.0/24"]
+  }
+
+  ingress {
+    to_port     = 7001
+    from_port   = 7001
+    protocol    = "tcp"
+    cidr_blocks = ["192.164.0.0/24"]
+  }
+
+  # outbound internet access
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
